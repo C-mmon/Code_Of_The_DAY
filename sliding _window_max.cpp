5f24c915139a183a41f9049e6f268fa15b1be611
C-mmon <44610528+C-mmon@users.noreply.github.com>
2021-03-04 00:24:59 +0530

    Create sliding _window_max.cpp

---
+++ sliding _window_max.cpp
@@ -0,0 +1,9 @@
+vector <int> store;
+        int len=nums.size();
+        int max=INT_MIN;
+        for(int j=0;j<=len-k;j++)
+        {
+        store.push_back(*max_element(nums.begin()+j,nums.begin()+j+k));
+        }
+        return store;
+//naive must improve
